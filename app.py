2662387d6716742bd7e20135a555e43dff6def77
luan <luanbispo.sousa263@gmail.com>
2025-04-17 16:46:04 -0300

    - Ajustei formulário HTML para enviar o link corretamente (name="link_video") - Atualizei o app.py:     - Corrigi captura do link recebido via POST (request.form.get('link_video'))     - Implementei função 'analisar_video' que gera uma resposta simulada     - Ajustei render_template para enviar a variável 'resposta' para o HTML - Corrigi a exibição da resposta no template (condicional Jinja) - Estilizei o campo de resposta com box-shadow e melhorei o layout - Corrigi problema de limite e overflow do campo de link

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
+
 def home():
-    return render_template("index.html")
+    resposta = None
+    if request.method == 'POST':
+        link_video = request.form.get('link_video')
+        #chamar funcao de transcriçao
+        resposta = analisar_video(link_video)
+
+    return render_template("index.html" , resposta=resposta)
+
+def analisar_video(link_video):
+    #logica do analizador aqui
+    return f"resumo gerado {link_video}"
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
